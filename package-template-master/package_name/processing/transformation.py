1848b5d5a796ef9b9a557707a6d46e6b3a096b55
Otavio Augusto <otavioaugust@gmail.com>
2022-09-29 14:28:54 -0300

    Update transformation.py

--- package-template-master/package_name/processing/transformation.py
+++ package-template-master/package_name/processing/transformation.py
@@ -2,6 +2,13 @@
 from skimage.transform import resize
 
 def resize_image(image, proportion):
+    """
+  Pega uma imagem e uma proporção, e retorna uma imagem redimensionada
+    
+     :param image: a imagem a ser redimensionada
+     :param ratio: A proporção da imagem original para redimensionar
+     :return: A imagem está sendo redimensionada para a proporção especificada.
+    """
     assert 0 <= proportion <=1, "Specify a valid proportion between 0 and 1."
     height = round(image.shape[0]*proportion)
     width = round(image.shape[1]*proportion)
